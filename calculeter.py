89f328b61e469837e36e4569e71d44f8be6453b2
yahav-dahan <105238103+yahav-dahan@users.noreply.github.com>
2022-05-23 20:25:44 +0300

    Update calculeter.py

--- calculeter.py
+++ calculeter.py
@@ -3,17 +3,23 @@ num_2 = input('num 2:')
 operand = input('operand:')
 num_1 = float(num_1)
 num_2 = float(num_2)
+result = 'error'
+good_result = 'the answer is:'
 answer = -1
 if operand == '*':
     answer = num_1 * num_2
+    result = good_result + str(answer)
 elif operand == '+':
     answer = num_1 + num_2
+    result = good_result + str(answer)
 elif operand == '/':
-    answer = num_1 / num_2
+    if num_2 != 0:
+        answer = num_1 / num_2
+        result = good_result + str(answer)
 elif operand == '-':
     answer = num_1 - num_2
-else:
-    print('EROR')
-print('the answer is:'+ str (answer))
+    result = good_result + str(answer)
+
+print(result)
 
 
